a4d9d72d904405ec72323914acd4f24f68618f8f
Raymond <109781956+Raymond870307@users.noreply.github.com>
2024-08-03 14:52:20 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'hex2024-vue-week1', // 将 <REPO_NAME> 替换为你的 GitHub 仓库名称
 })
